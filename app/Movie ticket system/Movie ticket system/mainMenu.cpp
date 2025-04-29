cc075318f525db2ab71a14d5181bf6fd10315ba3
VTVaradeva22 <VTVaradeva22@codingburgas.bg>
2025-04-29 15:23:22 +0300

    Implement function startMenu

--- app/Movie ticket system/Movie ticket system/mainMenu.cpp
+++ app/Movie ticket system/Movie ticket system/mainMenu.cpp
@@ -54,3 +54,44 @@ public:
 		}
 	}
 };
+
+void startMenu() {
+	while (true) {
+		int mainCheck = 0;
+		int newCheck = 0;
+		system("cls");
+		//aboutPgae();
+		cin >> newCheck;
+		system("cls");
+		if (newCheck == 1) {
+			cout << " Welcome to..." << endl;
+			cout << " 1. Administrator" << endl;
+			cout << " 2. Customer" << endl;
+			cin >> mainCheck;
+			if (mainCheck == 1) {
+				char userName[100];
+				char pass[100];
+				cout << "Enter username: ";
+				cin >> userName;
+				cout << "Enter password: ";
+				cin >> pass;
+				char user[100];
+				char userPass[100];
+				int nCheck = 0;
+				int aCheck = 0;
+				//ifstrean userData("admins.txt");
+				{
+					if (!userData) {
+						cout << "Cannot open files!";
+					}
+					while (!(userData.eof())) {
+						userData >> user >> userPass;
+						if (compare(user, userName)) {
+
+						}
+					}
+				}
+			}
+		}
+	}
+}
