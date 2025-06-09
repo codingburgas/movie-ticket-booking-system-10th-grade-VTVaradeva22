44d0efb5884110d4414aaad4d456f948945d580d
Veselina <VTVaradeva22@codingburgas.bg>
2025-06-09 18:07:41 +0300

    Add colours to Admin and User headings

--- app/Cinema system/Cinema system/admin.cpp
+++ app/Cinema system/Cinema system/admin.cpp
@@ -3,43 +3,50 @@
 #include <iostream>
 using namespace std;
 
-
-
 void showAdminMenu(const string& username) {
+    string purpleColor = "\033[35m"; 
+    string whiteColor = "\033[37m"; 
+    string redColor = "\033[31m";    
+    string resetColor = "\033[0m";   
+
     int choice;
     do {
-        cout << "\n";
-        centerText( "===========================================");
-        centerText( "             ADMIN MENU                    ");
-        centerText( "===========================================");
+        printEndl(1);
+        centerText(purpleColor + "===========================================" + resetColor);
+        centerText(purpleColor + "             ADMIN MENU                    " + resetColor);
+        centerText(purpleColor + "===========================================" + resetColor);
+        printEndl(1);
 
-        cout << "1. Add movie\n";
-        cout << "2. Remove movie\n";
-        cout << "3. View movies\n";
-        cout << "4. Book a movie\n";
-        cout << "5. Logout\n";
-        cout << "Choice: ";
+        centerText(whiteColor + "1. Add movie" + resetColor);
+        centerText(whiteColor + "2. Remove movie" + resetColor);
+        centerText(whiteColor + "3. View movies" + resetColor);
+        centerText(whiteColor + "4. Book a movie" + resetColor);
+        centerText(whiteColor + "5. Logout" + resetColor);
+        printEndl(1);
+
+        printStrRepeat(" ", 2);
+        cout << whiteColor << "Choice: " << resetColor;
         cin >> choice;
 
         switch (choice) {
         case 1:
-           // addMovie();
+            // addMovie();
             break;
         case 2:
-            //removeMovie();
+            // removeMovie();
             break;
         case 3:
-            //viewMovies();
+            // viewMovies();
             break;
         case 4:
-           // bookMovie(username); 
+            // bookMovie(username);
             break;
         case 5:
-            cout << "Logging out and returning to the main screen...\n";
+            cout << whiteColor << "Logging out and returning to the main screen..." << resetColor << endl;
             system("pause");
-            return; 
+            return;
         default:
-            cout << "Invalid choice! Enter a number between 1 and 5.\n";
+            cout << redColor << "Invalid choice! Enter a number between 1 and 5." << resetColor << endl;
             system("pause");
             break;
         }
--- app/Cinema system/Cinema system/user.cpp
+++ app/Cinema system/Cinema system/user.cpp
@@ -3,36 +3,42 @@
 #include <iostream>
 using namespace std;
 
-
 void showUserMenu(const string& username) {
+    string purpleColor = "\033[35m"; 
+    string whiteColor = "\033[37m";  
+    string redColor = "\033[31m";    
+    string resetColor = "\033[0m";   
 
     int choice;
     do {
-        cout << "\n";
-        centerText( "===========================================" );
-        centerText( "             USER MENU                     " );
-        centerText( "===========================================" );
+        printEndl(1);
+        centerText(purpleColor + "===========================================" + resetColor);
+        centerText(purpleColor + "             USER MENU                     " + resetColor);
+        centerText(purpleColor + "===========================================" + resetColor);
+        printEndl(1);
 
+        centerText(whiteColor + "1. View movies" + resetColor);
+        centerText(whiteColor + "2. Book a movie" + resetColor);
+        centerText(whiteColor + "3. Logout" + resetColor);
+        printEndl(1);
 
-        cout << "1. View movies\n";
-        cout << "2. Book a movie\n";
-        cout << "3. Logout\n";
-        cout << "Choice: ";
+        printStrRepeat(" ", 2);
+        cout << whiteColor << "Choice: " << resetColor;
         cin >> choice;
 
         switch (choice) {
         case 1:
-           // viewMovies();
+            // viewMovies();
             break;
         case 2:
-            //bookMovie(username);
+            // bookMovie(username);
             break;
         case 3:
-            cout << "Logging out and returning to the main screen...\n";
+            cout << whiteColor << "Logging out and returning to the main screen..." << resetColor << endl;
             system("pause");
-            return; 
+            return;
         default:
-            cout << "Invalid choice! Enter a number between 1 and 5.\n";
+            cout << redColor << "Invalid choice! Enter a number between 1 and 3." << resetColor << endl;
             system("pause");
             break;
         }
